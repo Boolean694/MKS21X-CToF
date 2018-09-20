a08c430d1b946a49778e3d9704c202cfcf015456
Julian Wong <jwong01@stuy.edu>
2018-09-19 20:09:41 -0400

    fix bugs

--- CToFTester.java
+++ CToFTester.java
@@ -1,8 +1,8 @@
-public class CtoFTester {
-	public double celsiusToFahrenheit(double deg) {
+public class CToFTester {
+	public static double celsiusToFahrenheit(double deg) {
 		return (deg * 9) / 5 + 32;
 	}
-	public double fahrenheitToCelsius(double deg) {
+	public static double fahrenheitToCelsius(double deg) {
 		return (deg - 32) * 5 / 9;
 	}
 	public static void main(String args[]) {
